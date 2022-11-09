28c6bca8643f229c0d24aeb87d5989e04d2234c8
Nimish Wadekar <wadekar.nimish@gmail.com>
2022-11-09 16:55:41 +0800

    Implement trywait2()

--- src/rust/inetstack/mod.rs
+++ src/rust/inetstack/mod.rs
@@ -496,11 +496,35 @@ impl InetStack {
         }
     }
 
+    /// Checks if an operation has completed and returns the result if it has.
+    pub fn trywait2(&mut self, qt: QToken) -> Result<Option<(QDesc, OperationResult)>, Fail> {
+        #[cfg(feature = "profiler")]
+        timer!("inetstack::trywait2");
+        trace!("trywait2(): qt={:?}", qt);
+
+        // Retrieve associated schedule handle.
+        let handle: SchedulerHandle = match self.scheduler.from_raw_handle(qt.into()) {
+            Some(handle) => handle,
+            None => return Err(Fail::new(libc::EINVAL, "invalid queue token")),
+        };
+
+        // Poll first, so as to give pending operations a chance to complete.
+        self.poll_bg_work();
+
+        // The operation has completed, so extract the result and return.
+        if handle.has_completed() {
+            trace!("wait2() qt={:?} completed!", qt);
+            Ok(Some(self.take_operation(handle)))
+        } else {
+            Ok(None)
+        }
+    }
+
     /// Waits for an I/O operation to complete or a timeout to expire.
     pub fn timedwait2(&mut self, qt: QToken, abstime: Option<SystemTime>) -> Result<(QDesc, OperationResult), Fail> {
         #[cfg(feature = "profiler")]
-        timer!("inetstack::timedwait");
-        trace!("timedwait() qt={:?}, timeout={:?}", qt, abstime);
+        timer!("inetstack::timedwait2");
+        trace!("timedwait2() qt={:?}, timeout={:?}", qt, abstime);
 
         // Retrieve associated schedule handle.
         let mut handle: SchedulerHandle = match self.scheduler.from_raw_handle(qt.into()) {
