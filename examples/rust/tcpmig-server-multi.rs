847be536b9f8db35f089cb1f782fd48117d454ee
ihchoi12 <inho.choi.12@gmail.com>
2023-05-09 12:14:26 +0000

    tcpmig-server-multi print out performance logs before finishing

--- examples/rust/tcpmig-server-multi.rs
+++ examples/rust/tcpmig-server-multi.rs
@@ -27,6 +27,9 @@ use ::std::{
 #[cfg(feature = "profiler")]
 use ::demikernel::perftools::profiler;
 
+use std::sync::atomic::{AtomicBool, Ordering};
+use ctrlc;
+use std::sync::Arc;
 //======================================================================================================================
 // server()
 //======================================================================================================================
@@ -61,6 +64,12 @@ fn send_response(libos: &mut LibOS, qd: QDesc, data: &[u8]) -> QToken {
 }
 
 fn server(local: SocketAddrV4) -> Result<()> {
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");
+
     let libos_name: LibOSName = match LibOSName::from_env() {
         Ok(libos_name) => libos_name.into(),
         Err(e) => panic!("{:?}", e),
@@ -91,7 +100,7 @@ fn server(local: SocketAddrV4) -> Result<()> {
     let mut migratable_qds: HashMap<QDesc, QToken> = HashMap::new();
 
     loop {
-        if qts.is_empty() {
+        if qts.is_empty() || !running.load(Ordering::SeqCst) {
             break;
         }
 
@@ -159,13 +168,14 @@ fn server(local: SocketAddrV4) -> Result<()> {
 
     eprintln!("server stopping");
 
-    loop {}
+    // loop {}
 
     #[cfg(feature = "profiler")]
     profiler::write(&mut std::io::stdout(), None).expect("failed to write to stdout");
-
+    #[cfg(feature = "tcp-migration-profiler")]
+    demikernel::tcpmig_profiler::write_profiler_data(&mut std::io::stdout()).unwrap();
     // TODO: close socket when we get close working properly in catnip.
-    //Ok(())
+    Ok(())
 }
 
 //======================================================================================================================
@@ -183,6 +193,9 @@ fn usage(program_name: &String) {
 
 pub fn main() -> Result<()> {
     logging::initialize();
+    #[cfg(feature = "tcp-migration-profiler")]
+    demikernel::tcpmig_profiler::init_profiler();
+
     let args: Vec<String> = env::args().collect();
 
     if args.len() >= 2 {
