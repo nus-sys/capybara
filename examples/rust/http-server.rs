1bc07f2395fc4ffa13017ba0bff7c4bde8f9fa0b
Josh Fried <joshuafried@gmail.com>
2023-06-13 21:30:36 -0600

    http server updates
    add support for receiving requests that are fragmented across
    packets (i.e. when TCP windows are full)

--- examples/rust/http-server.rs
+++ examples/rust/http-server.rs
@@ -32,13 +32,73 @@ const ROOT: &str = "/var/www/demo";
 use std::sync::atomic::{AtomicBool, Ordering};
 use ctrlc;
 use std::sync::Arc;
-//======================================================================================================================
-// server()
-//======================================================================================================================
 
-fn send_response(libos: &mut LibOS, qd: QDesc, data: &[u8]) -> QToken {
+const BUFSZ: usize = 4096;
+
+// Borrowed from Loadgen
+struct Buffer {
+    buf: Vec<u8>,
+    head: usize,
+    tail: usize,
+}
+
+impl Buffer {
+    pub fn new() -> Buffer {
+        Buffer {
+            buf: vec![0; BUFSZ],
+            head: 0,
+            tail: 0,
+        }
+    }
+
+    pub fn data_size(&self) -> usize {
+        self.head - self.tail
+    }
+
+    pub fn get_data(&self) -> &[u8] {
+        &self.buf[self.tail..self.head]
+    }
+
+    pub fn push_data(&mut self, size: usize) {
+        self.head += size;
+        assert!(self.head <= self.buf.len());
+    }
+
+    pub fn pull_data(&mut self, size: usize) {
+        assert!(size <= self.data_size());
+        self.tail += size;
+    }
+
+    pub fn get_empty_buf(&mut self) -> &mut [u8] {
+        &mut self.buf[self.head..]
+    }
+
+    pub fn try_shrink(&mut self) -> Result<()> {
+        if self.data_size() == 0 {
+            self.head = 0;
+            self.tail = 0;
+            return Ok(());
+        }
+
+        if self.head < self.buf.len() {
+            return Ok(());
+        }
+
+        if self.data_size() == self.buf.len() {
+            panic!("Need larger buffer for HTTP messages");
+        }
+
+        self.buf.copy_within(self.tail..self.head, 0);
+        self.head = self.data_size();
+        self.tail = 0;
+        Ok(())
+    }
+}
+
+fn respond_to_request(libos: &mut LibOS, qd: QDesc, data: &[u8]) -> QToken {
     // let data_str = std::str::from_utf8(data).unwrap();
-    let data_str = String::from_utf8_lossy(data);
+    // let data_str = String::from_utf8_lossy(data);
+    let data_str = unsafe { std::str::from_utf8_unchecked(&data[..]) };
 
     let mut file_name = data_str
             .split_whitespace()
@@ -65,9 +125,58 @@ fn send_response(libos: &mut LibOS, qd: QDesc, data: &[u8]) -> QToken {
     libos.push2(qd, response.as_bytes()).expect("push success")
 }
 
+#[inline(always)]
+fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
+    haystack
+        .windows(needle.len())
+        .position(|window| window == needle)
+}
+
+fn push_data_and_run(libos: &mut LibOS, qd: QDesc, buffer: &mut Buffer, data: &[u8], qts: &mut Vec<QToken>) -> usize {
+
+    // fast path: no previous data in the stream and this request contains exactly one HTTP request
+    if buffer.data_size() == 0 {
+        if find_subsequence(data, b"\r\n\r\n").unwrap_or(data.len()) == data.len() - 4 {
+            let resp_qt = respond_to_request(libos, qd, data);
+            qts.push(resp_qt);
+            return 1;
+        }
+    }
+
+    // Copy new data into buffer
+    buffer.get_empty_buf()[..data.len()].copy_from_slice(data);
+    buffer.push_data(data.len());
+
+    let mut sent = 0;
+
+    loop {
+        let dbuf = buffer.get_data();
+        match find_subsequence(dbuf, b"\r\n\r\n") {
+            Some(idx) => {
+                let resp_qt = respond_to_request(libos, qd, &dbuf[..idx + 4]);
+                qts.push(resp_qt);
+                buffer.pull_data(idx + 4);
+                buffer.try_shrink().unwrap();
+                sent += 1;
+            }
+            None => {
+                return sent;
+            }
+        }
+    }
+}
+
+
+//======================================================================================================================
+// server()
+//======================================================================================================================
+
+
 struct ConnectionState {
-    pushing: bool,
+    pushing: usize,
     pop_qt: QToken,
+    buffer: Buffer,
+
 }
 
 fn server(local: SocketAddrV4) -> Result<()> {
@@ -81,8 +190,8 @@ fn server(local: SocketAddrV4) -> Result<()> {
     let mut libos: LibOS = LibOS::new(libos_name).expect("intialized libos");
     let sockqd: QDesc = libos.socket(libc::AF_INET, libc::SOCK_STREAM, 0).expect("created socket");
 
-    libos.bind(sockqd, local).expect("bind failed");
-    libos.listen(sockqd, 16).expect("listen failed");
+    libos.bind(sockqd, local).expect("bind socket");
+    libos.listen(sockqd, 16).expect("listen socket");
 
     let mut qts: Vec<QToken> = Vec::new();
     let mut connstate: HashMap<QDesc, ConnectionState> = HashMap::new();
@@ -104,26 +213,26 @@ fn server(local: SocketAddrV4) -> Result<()> {
                     let pop_qt = libos.pop(new_qd).expect("pop qt");
                     qts.push(pop_qt);
                     connstate.insert(new_qd, ConnectionState {
-                        pushing: false,
+                        pushing: 0,
                         pop_qt: pop_qt,
+                        buffer: Buffer::new(),
                     });
 
                     // Re-arm accept
                     qts.push(libos.accept(qd).expect("accept qtoken"));
                 },
                 OperationResult::Push => {
-                    connstate.get_mut(&qd).unwrap().pushing = false;
+                    connstate.get_mut(&qd).unwrap().pushing -= 1;
                 },
                 OperationResult::Pop(_, recvbuf) => {
-                    let send_qt = send_response(&mut libos, qd, &recvbuf);
-                    qts.push(send_qt);
+                    let mut state = connstate.get_mut(&qd).unwrap();
+                    let sent = push_data_and_run(&mut libos, qd, &mut state.buffer, &recvbuf, &mut qts);
 
                     // queue next pop
                     let pop_qt = libos.pop(qd).expect("pop qt");
                     qts.push(pop_qt);
 
-                    let mut state = connstate.get_mut(&qd).unwrap();
-                    state.pushing = true;
+                    state.pushing += sent;
                     state.pop_qt = pop_qt;
                 },
                 _ => {
@@ -137,7 +246,8 @@ fn server(local: SocketAddrV4) -> Result<()> {
             let mut qd_to_remove = None;
 
             for (qd, state) in connstate.iter() {
-                if state.pushing {
+                // Can't migrate a connection with outstanding TX or partially processed HTTP requests in the TCP stream
+                if state.pushing > 0 || state.buffer.data_size() > 0 {
                     continue;
                 }
                 match libos.notify_migration_safety(*qd) {
