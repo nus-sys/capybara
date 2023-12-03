5c2ab9aa8046054816124bcbfc9824c5bb67a641
ihchoi12 <inho.choi.12@gmail.com>
2023-12-03 09:17:26 +0000

    fix pkill in run_eval

--- eval/test_config.py
+++ eval/test_config.py
@@ -17,7 +17,7 @@ FRONTEND_NODE = 'node8'
 ################## BUILD CONFIG #####################
 FEATURES = [
     'tcp-migration',
-    # 'capy-log',
+    'capy-log',
     #'capy-profile',
     #'capy-time-log'
 ]
@@ -25,8 +25,8 @@ FEATURES = [
 ################## TEST CONFIG #####################
 NUM_BACKENDS = 4
 SERVER_APP = 'http-server'
-CLIENT_APP = 'wrk'
-# CLIENT_APP = 'caladan'
+# CLIENT_APP = 'wrk'
+CLIENT_APP = 'caladan'
 FE_APP = 'prism-fe'
 BE_APP = 'prism-be-http'
 REPEAT_NUM = 1
@@ -34,10 +34,10 @@ RECV_QUEUE_THRESHOLD = 100
 MIG_DELAYS = [0] 
 MAX_STAT_MIGS = [0]#[5000, 10000, 15000] # set element to '' if you don't want to set this env var
 MIG_PER_N = [1]#[5000, 10000, 15000, 20000, 25000, 30000, 40000, 50000, 70000]
-CLIENT_PPS = [1]#[i for i in range(1000, 30000 + 1, 3000)]#[i for i in range(100000, 1_300_001, 100000)]
-NUM_CONNECTIONS = [9]#[i for i in range(1, 10 + 1, 2)]
-NUM_THREADS = [5, 6]#[i for i in range(1, 5 + 1, 1)]
-RUNTIME = 10
+CLIENT_PPS = [i for i in range(1000, 30000 + 1, 3000)]#[i for i in range(100000, 1_300_001, 100000)]
+NUM_CONNECTIONS = [1]#[i for i in range(1, 10 + 1, 2)]
+NUM_THREADS = [1]#[i for i in range(1, 5 + 1, 1)]
+RUNTIME = 2
 TCPDUMP = False
 EVAL_MIG_LATENCY = False
 EVAL_POLL_INTERVAL = False
